1e9825e538b170e9b156c1eae47841d0360fde38
Huangjianhao <1421196400@qq.com>
2021-01-15 10:41:29 +0800

    update

---
+++ crud_demo/components/Search.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="search-card">
+    <p class="input-title">姓名</p>
+    <Input
+      v-model="userv.name"
+      class="search-input"
+      placeholder="根据姓名查询"
+      clearable
+    />
+    <p class="input-title">年龄</p>
+    <Input
+      v-model="userv.age"
+      class="search-input"
+      type="number"
+      placeholder="根据年龄查询"
+    />
+    <p class="input-title">地址</p>
+    <Input
+      v-model="userv.address"
+      class="search-input"
+      placeholder="根据地址查询"
+      clearable
+    />
+    <Button @click="searchBtn">搜索</Button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  // eslint-disable-next-line vue/require-default-prop
+  props: { listt: Array },
+  data() {
+    return {
+      userv: { name: '', age: '', address: '' }, // ? 搜索框中绑定的姓名、年龄、地址
+    }
+  },
+  methods: {
+    searchBtn() {
+      this.$emit('getList', this.userv.name, this.userv.age, this.userv.address)
+    },
+  },
+}
+</script>
+
+<style scoped>
+@import url('../assets/main');
+</style>
--- crud_demo/pages/index.vue
+++ crud_demo/pages/index.vue
@@ -1,54 +1,10 @@
-/* eslint-disable no-unused-expressions */
 <template>
   <div class="container">
     <!-- 搜索框 -->
-    <div class="search-card">
-      <p class="input-title">姓名</p>
-      <Input
-        v-model="userv.name"
-        class="search-input"
-        placeholder="根据姓名查询"
-        clearable
-      />
-      <p class="input-title">年龄</p>
-      <Input
-        v-model="userv.age"
-        class="search-input"
-        type="number"
-        placeholder="根据年龄查询"
-      />
-      <p class="input-title">地址</p>
-      <Input
-        v-model="userv.address"
-        class="search-input"
-        placeholder="根据地址查询"
-        clearable
-      />
-    </div>
+    <Search :listt="data" @getList="getList"></Search>
     <!-- 主要内容 -->
     <div class="main-card">
-      <Button class="add-btn" @click="addBtn()">
-        <Icon class="add-icon" type="md-add" />
-      </Button>
-      <!-- 新增弹窗 -->
-      <Modal
-        v-model="modalAdd"
-        ok-text="添加"
-        cancel-text="取消"
-        @on-ok="addModal"
-      >
-        <p class="input-title">姓名</p>
-        <Input v-model="userAdd.name" placeholder="请输入姓名" clearable />
-        <p class="input-title">年龄</p>
-        <Input
-          v-model="userAdd.age"
-          type="number"
-          placeholder="请输入年龄"
-          clearable
-        />
-        <p class="input-title">地址</p>
-        <Input v-model="userAdd.address" placeholder="请输入地址" clearable />
-      </Modal>
+      <AddModal :listt="data"></AddModal>
       <!-- 表格 -->
       <Table height="450" :columns="columns" :data="list">
         <template slot="name" slot-scope="{ row }">
@@ -61,41 +17,8 @@
           <span>{{ row.address }}</span>
         </template>
         <template slot="action" slot-scope="{ row, index }">
-          <div>
-            <Button
-              type="primary"
-              size="small"
-              class="modify-btn"
-              @click="modifyBtn(row, index)"
-            >
-              修改
-            </Button>
-            <Modal
-              v-model="editModal"
-              title="修改"
-              ok-text="保存"
-              cancel-text="取消"
-              @on-ok="saveBtn(indexx)"
-            >
-              <p class="input-title">姓名</p>
-              <Input
-                v-model="userEdit.name"
-                placeholder="请输入姓名"
-                clearable
-              />
-              <p class="input-title">年龄</p>
-              <Input
-                v-model="userEdit.age"
-                type="number"
-                placeholder="请输入年龄"
-              />
-              <p class="input-title">地址</p>
-              <Input
-                v-model="userEdit.address"
-                placeholder="请输入地址"
-                clearable
-              />
-            </Modal>
+          <div class="action">
+            <EditModal :row="row" :index="index" :listt="data"></EditModal>
             <Button
               class="delete-btn"
               type="error"
@@ -112,15 +35,18 @@
 </template>
 
 <script>
+import Search from '../components/Search'
+import AddModal from '../components/AddModal'
+import EditModal from '../components/EditModal'
 export default {
+  components: {
+    Search,
+    AddModal,
+    EditModal,
+  },
   data() {
     return {
-      indexx: '',
-      userv: { name: '', age: '', address: '' }, // ? 搜索框中绑定的姓名、年龄、地址
-      userAdd: { name: '', age: '', address: '' }, // ? 新增弹窗中绑定的姓名、年龄、地址
-      userEdit: { name: '', age: '', address: '' }, // ? 修改弹窗中绑定的姓名、年龄、地址
-      modalAdd: false,
-      editModal: false,
+      userSearch: { name: '', age: '', address: '' },
       columns: [
         {
           title: '姓名',
@@ -171,54 +97,17 @@ export default {
     list() {
       return this.data.filter(
         (x) =>
-          x.name.includes(this.userv.name) &&
-          x.age.toString().includes(this.userv.age) &&
-          x.address.includes(this.userv.address)
+          x.name.includes(this.userSearch.name) &&
+          x.age.toString().includes(this.userSearch.age) &&
+          x.address.includes(this.userSearch.address)
       )
     },
   },
   methods: {
-    // ? 保存按钮
-    saveBtn(index) {
-      if (this.userEdit.name && this.userEdit.age && this.userEdit.address) {
-        this.data[index].name = this.userEdit.name
-        this.data[index].age = this.userEdit.age
-        this.data[index].address = this.userEdit.address
-        this.$Message.success('修改成功')
-      } else {
-        this.$Message.error('修改失败，内容不能为空')
-      }
-    },
-    // ? 新增按钮
-    addBtn() {
-      this.modalAdd = true
-      this.userAdd.name = ''
-      this.userAdd.age = ''
-      this.userAdd.address = ''
-    },
-    // ? 添加弹窗中的添加按钮
-    addModal() {
-      if (this.userAdd.name && this.userAdd.age && this.userAdd.address) {
-        this.data.push({
-          name: this.userAdd.name,
-          age: Number(this.userAdd.age),
-          address: this.userAdd.address,
-        })
-        this.$Message.success('添加成功')
-      } else {
-        this.$Message.error('添加失败,内容不能为空！')
-      }
-      this.userAdd.age = ''
-      this.userAdd.name = ''
-      this.userAdd.address = ''
-    },
-    // ?修改按钮
-    modifyBtn(row, index) {
-      this.editModal = true
-      this.userEdit.name = row.name
-      this.userEdit.age = row.age
-      this.userEdit.address = row.address
-      this.indexx = index
+    getList(userName, userAge, userAddress) {
+      this.userSearch.name = userName
+      this.userSearch.age = userAge
+      this.userSearch.address = userAddress
     },
     // ?删除按钮
     remove(index) {
